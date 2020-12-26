b5fff1ac88e40eed5eb9edcd51c71d81a8ef5e94
Nathaniel May <nate.may@fauna.com>
2020-12-26 16:27:26 -0500

    uses random numbers

--- src/Main.purs
+++ src/Main.purs
@@ -3,7 +3,11 @@ module Main where
 import Prelude
 import Effect (Effect)
 import Effect.Console (log)
+import Effect.Random
 
 main :: Effect Unit
 main = do
-  log "Hello sailor!"
+  n <- random
+  log "random number:"
+  log (show n)
+
